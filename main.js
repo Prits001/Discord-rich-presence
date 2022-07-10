b7ef93d6161edd229d8e27a9277cfad9186e9372
Prits001 <ivan.prits.80@mail.ru>
2022-07-10 14:06:36 +0300

    Colorful console update

--- main.js
+++ main.js
@@ -7,18 +7,29 @@ const DiscordApi = require('discord-rpc');
 const { time } = require('console');
 const Api = new DiscordApi.Client({ transport: 'ipc'})
 DiscordApi.register(clientId);
-function Console(){
-    console.log("FireAlpaca Rich Presence - by Prits#2138 \n-----------------------------------------------------")
-    console.log("Connecting to Discord")
-    setTimeout(function(){console.log("[/....]")}, 1200);
-    setTimeout(function(){console.log("[#/...]")}, 1200);
-    setTimeout(function(){console.log("[##/..]")}, 1200);
-    setTimeout(function(){console.log("[###/.]")}, 1200);
-    setTimeout(function(){console.log("[####/]")}, 1200);
-    setTimeout(function(){console.log("[#####]");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function checkConfig(){
+    if(config.clientId === '' || config.drawingname === '' || config.additionalinfo === '' || config.largeimagetext === ''){return false}
+    console.log('\x1b[36m%s\x1b[0m', "[!]", "\x1b[32mCurrent config settings:\x1b[0m")
+    console.log('\x1b[36m%s\x1b[0m',"[#]", "Application id: ", "\x1b[32m"+config.clientId+"\x1b[0m")
+    console.log('\x1b[36m%s\x1b[0m',"[#]", "Drawing name: ", "\x1b[32m"+config.drawingname+"\x1b[0m")
+    console.log('\x1b[36m%s\x1b[0m',"[#]", "Additional info: ", "\x1b[32m"+config.additionalinfo+"\x1b[0m")
+    console.log('\x1b[36m%s\x1b[0m',"[#]", "Text when hovering above large image icon: ","\x1b[32m"+config.largeimagetext+"\x1b[0m")
+    console.log("Starting in 10 seconds..")
+    console.log("\x1b[46mPress Ctrl+C to abort\x1b[0m")
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 10000);
+    return true
+}
+function start(){
+    console.log("\x1b[35mFireAlpaca Rich Presence - by Prits#2138\x1b[0m")
+    console.log("-----------------------------------------------------")
+    if(!checkConfig()){throw new Error("Config Settings were terminated!");}
+    console.log("Connecting to Discord...")
     console.clear()
-    console.log("FireAlpaca Rich Presence - by Prits#2138 \n-----------------------------------------------------")
-    console.log("Successfully Connected to Discord Gateway! :D \nPress Ctrl+C to abort")}, 5000);
+    console.log("\x1b[35mFireAlpaca Rich Presence - by Prits#2138\x1b[0m \n-----------------------------------------------------")
+    console.log("\x1b[32mSuccessfully\x1b[0m Connected to Discord Gateway! :D \n\x1b[46mPress Ctrl+C to abort\x1b[0m")
     
 }
 
@@ -35,6 +46,7 @@ async function setActivity() {
         instance: false,
     });
 };
+start()
 Api.on('ready', async () => {
     setActivity()})
-Api.login(({ clientId })).catch(err => console.error(err)).then(Console())
+Api.login(({ clientId })).catch(err => console.error(err)).then()
